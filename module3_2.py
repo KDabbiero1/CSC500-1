a93295afd3f873927a3c0dd18f97fad6f5274f0f
KDabbiero1 <157320248+KDabbiero1@users.noreply.github.com>
2024-02-04 22:23:30 -0700

    Update module3_2.py

--- module3_2.py
+++ module3_2.py
@@ -20,25 +20,27 @@ def calc_start_24_hour_time(currentTime):
     
 def calc_end_24_hour_time(currentTime, hours):
     total_time = currentTime + hours
+    end_time24 = total_time % 24
     mod = total_time % 24
     if mod == 0:
-        end_time = 12
+        end_time12 = 12
         end_am_pm = 'am'
     elif mod < 12:
-        end_time = mod
+        end_time12 = mod
         end_am_pm = 'am'
     elif mod == 12:
-        end_time = mod
+        end_time12 = mod
         end_am_pm = 'pm'
     else:
-        end_time = mod - 12
+        end_time12 = mod - 12
         end_am_pm = 'pm'
-    return end_time, end_am_pm
+    return end_time24, end_time12, end_am_pm
 
 def main():
     currentTime, hours = get_current_time_and_hours()
     start_time, start_am_pm = calc_start_24_hour_time(currentTime)
-    end_time, end_am_pm = calc_end_24_hour_time(currentTime, hours)
-    print(f"Current time:{start_time} {start_am_pm} + {hours} hours\nAlarm Time: {end_time} {end_am_pm} ")
+    end_time24, end_time12, end_am_pm = calc_end_24_hour_time(currentTime, hours)
+    print(f"Current time:{start_time} {start_am_pm} + {hours} hours\nAlarm Time (12h): {end_time12}{end_am_pm}\nAlarm Time (24h): {end_time24}")
+    
 
 main()
